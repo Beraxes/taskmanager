e6c6258843b21b762a72159587053a1bd1cb4773
Javier Andres Olivera Soto <85058260+Beraxes@users.noreply.github.com>
2025-01-15 21:19:44 -0500

    chore: added some .env config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,17 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: process.env.MONGO_URI || 'mongodb://nest:nest123@localhost:27017',
+      url: process.env.MONGO_URI, //|| 'mongodb://nest:nest123@localhost:27017',
       synchronize: true, // Sincroniza automáticamente las entidades con la base de datos (no recomendado en producción)
       useUnifiedTopology: true,
     }),
     UserModule,
     TaskModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,21 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+dotenv.config({ path: '.env' });
+
 async function bootstrap() {
-  console.log('Mongo URI:', process.env.MONGO_URI); // Verificar que la URI se carga
+  console.log('Loading environment variables...');
+  console.log('MONGO_URI:', process.env.MONGO_URI);
+  console.log('PORT:', process.env.PORT);
+
+  if (!process.env.MONGO_URI || !process.env.PORT) {
+    throw new Error('Environment variables MONGO_URI and PORT are not set');
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT);
 }
+
 bootstrap();
