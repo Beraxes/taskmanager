d35c93bf5511619b4ffafcc1838ee2968468afa8
Javier Andres Olivera Soto <85058260+Beraxes@users.noreply.github.com>
2025-02-05 14:50:00 -0500

    fix: rookie mistake i forgot to update the secretkey from Auth strategy leading to a unauthorized task creation

--- api/src/auth/jwt.strategy.ts
+++ api/src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secretKey', // Cambia esta clave en producción
+      secretOrKey: process.env.JWT_SECRET, // Cambia esta clave en producción
     });
   }
 
